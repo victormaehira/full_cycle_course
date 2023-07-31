656d1b008fda3bd1519a3bec1542ade34ef6b93b
Victor Maehira <vymaehira@gmail.com>
2023-07-30 23:28:30 -0300

    Env variables added.

--- fc-k8s/server.go
+++ fc-k8s/server.go
@@ -1,12 +1,18 @@
 package main
 
-import "net/http" 
+import (
+	"net/http" 
+	"os"
+	"fmt"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle v2</h1>"))
+	name :=  os.Getenv("NAME")
+	age := os.Getenv("AGE") 
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
